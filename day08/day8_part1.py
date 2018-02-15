4fe60d290bd3a3fd021fc081bfd5d53972f1d91a
Yuki <35814354+Miyooki@users.noreply.github.com>
2018-02-15 03:18:48 -0500

    adventofcode2017

--- day08/day8_part1.py
+++ day08/day8_part1.py
@@ -6,21 +6,14 @@ for line in file:
     commands.append(line.strip().split(" "))
 
 register = {}
-largest = 0
 
 def reg_change(command):
-    global largest
     if command[1] == "inc":
         register[command[0]] += int(command[2])
-        compare = register[command[0]]
-        if compare > largest:
-            largest = compare
 
     elif command[1] == "dec":
         register[command[0]] -= int(command[2])
-        compare = register[command[0]]
-        if compare > largest:
-            largest = compare
+
 
 def reg_help(command):
     if register.get(command[0]) == None:
@@ -52,7 +45,8 @@ def reg_help(command):
         if register[command[4]] >= int(command[6]):
             reg_change(command)
 
+
 for command in commands:
     reg_help(command)
 
-print(largest)
+print("Highest value held in any register:", register[max(register, key=register.get)])
--- day08/day8_part2.py
+++ day08/day8_part2.py
@@ -6,14 +6,21 @@ for line in file:
     commands.append(line.strip().split(" "))
 
 register = {}
+largest = 0
 
 def reg_change(command):
+    global largest
     if command[1] == "inc":
         register[command[0]] += int(command[2])
+        compare = register[command[0]]
+        if compare > largest:
+            largest = compare
 
     elif command[1] == "dec":
         register[command[0]] -= int(command[2])
-
+        compare = register[command[0]]
+        if compare > largest:
+            largest = compare
 
 def reg_help(command):
     if register.get(command[0]) == None:
@@ -45,8 +52,7 @@ def reg_help(command):
         if register[command[4]] >= int(command[6]):
             reg_change(command)
 
-
 for command in commands:
     reg_help(command)
 
-print(register[max(register, key=register.get)])
+print("Highest value held in any register during the whole process:", largest)
